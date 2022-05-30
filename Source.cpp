f556e0d20bb29c1217406e9ddceb061d90a63e4d
4isper <90023626+4isper@users.noreply.github.com>
2022-05-30 20:22:55 +0300

    Добавить .gitattributes, .gitignore и README.md.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include "Quadrate.h"
+#include "Polygon.h"
+#include "Line.h"
+
+
+int main() {
+
+  TQuadrate<int> bebraQvadr(2);
+
+  bebraQvadr.Print();
+
+  TLine<int> lineMatr(2);
+  lineMatr.Print();
+
+  return 0;
+}
